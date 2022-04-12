5a678ba80db2576413250681479166ad5730acee
CaretSoftware <pbergsten@me.com>
2022-04-12 14:33:28 +0200

    Crafting

---
+++ SpelGrupp2/Assets/Scripts/Crafting.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crafting : MonoBehaviour {
+    public int copper;
+    public int transistor;
+    public int iron;
+    
+    void Start() {
+        
+    }
+    
+    void Update() {
+        
+    }
+}
