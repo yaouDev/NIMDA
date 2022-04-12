6e4c1dbb122e455a94229859a45a100cfa517461
yaouDev <augustjansson@outlook.com>
2022-04-12 23:14:51 +0200

    crafting repair

--- SpelGrupp2/Assets/Scripts/caej/Craft.cs
+++ SpelGrupp2/Assets/Scripts/caej/Craft.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Craft : MonoBehaviour
+public class Craft
 {
     public void CraftRecipe(Recipe recipe, Crafting crafting)
     {
