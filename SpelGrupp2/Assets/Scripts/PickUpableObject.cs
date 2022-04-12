6ab75e499e9a05f9e0ec7d34106eace7d71cfae5
augustheden <aggeh@hotmail.se>
2022-04-12 11:14:51 +0200

    pickUp

---
+++ SpelGrupp2/Assets/Scripts/PickUpableObject.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+ * Pickup-able objects require a rigidbody.
+ * The name "PlayerPlaceHolder" refers to the players child object where picked-up object will appear.
+ * Currently works on mouse objects.
+ * */
+public class PickUpableObject : MonoBehaviour
+{
+    [SerializeField] public Transform dest;
+    public Rigidbody rb;
+
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMouseDown()
+    {
+        rb.useGravity = false;
+        rb.freezeRotation = true;
+        transform.position = dest.position;
+        transform.parent = GameObject.Find("PlayerPlaceHolder").transform;
+    }
+
+    void OnMouseUp()
+    {
+        rb.useGravity = true;
+        rb.freezeRotation = false;
+        transform.parent = null;
+    }
+
+}
