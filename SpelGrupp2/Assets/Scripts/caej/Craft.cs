a9e1ac99e2c36475fed25395a5bf65226e49fe10
yaouDev <augustjansson@outlook.com>
2022-04-12 23:08:53 +0200

    primitive crafting

--- SpelGrupp2/Assets/Scripts/caej/Craft.cs
+++ SpelGrupp2/Assets/Scripts/caej/Craft.cs
@@ -6,21 +6,47 @@ public class Craft : MonoBehaviour
 {
     public void CraftRecipe(Recipe recipe, Crafting crafting)
     {
+        Debug.Log("Available copper: " + crafting.copper);
+        Debug.Log("Available iron: " + crafting.iron);
+        Debug.Log("Available transistor(s): " + crafting.transistor);
+
+        bool canCraft = true;
+
         if (recipe.product == null) Debug.LogWarning("Trying to craft null");
 
-        if (crafting.copper < recipe.copperNeeded) Debug.Log("Not enough copper!");
-        else if (crafting.iron < recipe.ironNeeded) Debug.Log("Not enough iron!");
-        else if (crafting.transistor < recipe.transistorNeeded) Debug.Log("Not enough transistors!");
-        else
+        //kan bytas ut mot en foreach loop ifall man vill ha många resources/använda resource objekt
+        if (crafting.copper < recipe.copperNeeded)
+        {
+            Debug.Log("Not enough copper!");
+            canCraft = false;
+        }
+        if (crafting.iron < recipe.ironNeeded)
+        {
+            Debug.Log("Not enough iron!");
+            canCraft = false;
+        }
+        if (crafting.transistor < recipe.transistorNeeded)
+        {
+            Debug.Log("Not enough transistors!");
+            canCraft = false;
+        }
+
+        if (canCraft)
         {
             crafting.copper -= recipe.copperNeeded;
             crafting.iron -= recipe.ironNeeded;
             crafting.transistor -= recipe.transistorNeeded;
 
-            //craft
+            if (recipe.product.name.Equals("Battery"))
+            {
+                //crafting.gameObject.GetComponent<BatteryUI>().AddBattery();
+                Debug.Log("YOU CRAFTED A BATTERY, NOW GO CRAFT AN ASSAULT");
+            }
+            else
+            {
+                Debug.Log("You crafted: " + recipe.product + "!");
+                Debug.Log("TODO: Implement desination for crafted items");
+            }
         }
-
-
     }
-
 }
