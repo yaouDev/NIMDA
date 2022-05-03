ce8fda434afe7074ee35f70070f34b8b1ed9f134
TheiPadCat <theipadcat@gmail.com>
2022-05-03 20:50:25 +0200

    GraphManager start

---
+++ SpelGrupp2/Assets/Scripts/Scripts_Emil/AI_Pathfinding/GraphManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphManager : MonoBehaviour {
+    
+    Dictionary<Vector3, Dictionary<Vector3>> masterGraph;
+
+    
+
+}
