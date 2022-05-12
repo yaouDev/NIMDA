809b1e43e49bf47b0ab515ffd491c6898676227a
WillowM <jema1654@student.su.se>
2022-05-12 19:05:24 +0200

    Compass & Fixes

--- SpelGrupp2/Assets/Scripts/SafeRoomCloseBehind.cs
+++ SpelGrupp2/Assets/Scripts/SafeRoomCloseBehind.cs
@@ -9,21 +9,25 @@ public class SafeRoomCloseBehind : MonoBehaviour {
     [SerializeField] private GameObject exit;
     private int playerCount;
     private EnemySpawnController spawnController;
+    private Compass compass;
 
     // Start is called before the first frame update
     void Start() {
         entranceClosed = entrance.transform.position;
         exitClosed = exit.transform.position;
         spawnController = GameObject.Find("EnemySpawnController").GetComponent<EnemySpawnController>();
+        compass = GameObject.Find("Compass").GetComponent<Compass>();
     }
 
     void OnTriggerEnter(Collider col) {
         if (col.gameObject.tag == "Player")
         {
             playerCount++;
-            Debug.Log("Playercount = " + playerCount);
+            //Debug.Log("Playercount = " + playerCount);
             if (playerCount == 2)
             {
+                CloseEntrance();
+                compass.UpdateQuest();
                 spawnController.GeneratorRunning(false);
                 CallbackSystem.EventSystem.Current.FireEvent(new CallbackSystem.SafeRoomEvent());
             }
