9ac73a6c8985add8d06deef211406d8f513a76cd
CaretSoftware <69549081+CaretSoftware@users.noreply.github.com>
2022-05-12 18:25:20 +0200

    Foliage, Camera,

--- SpelGrupp2/Assets/Scripts/TreeFader.cs
+++ SpelGrupp2/Assets/Scripts/TreeFader.cs
@@ -5,27 +5,105 @@ using UnityEngine;
 
 public class TreeFader : MonoBehaviour
 {
-    [SerializeField] private MeshRenderer[] meshRenderer;
     private Material[] materials;
-    [SerializeField][Range(0.0f, 1.0f)] private float transparency;
+    private bool fadingOut;
+    private float targetTransparency = 1.0f;
+    [SerializeField] private MeshRenderer[] meshRenderer;
+    [SerializeField] private float minTransparency = .1f;
+    [SerializeField][Range(0.0f, 1.0f)] private float transparency = 1.0f;
+
     private void Awake()
     {
-        //meshRenderer = GetComponentsInChildren<MeshRenderer>();
         materials = new Material[meshRenderer.Length];
         for (int i = 0; i < meshRenderer.Length; i++)
         {
             materials[i] = meshRenderer[i].material;
         }
     }
-
-    public float MinTransparency = .1f;
+    
     private void Update()
     {
-        float sin = Mathf.Max(MinTransparency, Mathf.Abs( Mathf.Sin(Time.time)));
+        if (fadingOut)
+        {
+            transparency -= Time.deltaTime;
+            
+            if (transparency <= minTransparency)
+            {
+                fadingOut = false;
+                transparency = minTransparency;
+            }
+            SetFade(transparency);
+        }
+        else if (transparency < 1.0f)
+        {
+            transparency += Time.deltaTime;
+            
+            if (transparency >= 1.0f)
+            {
+                transparency = 1.0f;
+            }
+            SetFade(transparency);
+        }
+    }
+
+    private void Debug()
+    {
+        float sin = Mathf.Max(minTransparency, Mathf.Abs( Mathf.Sin(Time.time)));
         transparency = sin;
         for (int i = 0; i < materials.Length; i++)
         {
             materials[i].SetFloat("_transparency", Mathf.Lerp(.5f, 1.5f, transparency));
+        }
+    }
+    
+    // public void FadeIn()
+    // {
+    //     fadingOut = false;
+    //     StopAllCoroutines();
+    //     StartCoroutine(FadeInCoroutine());
+    // }
+    
+    public void FadeOut()
+    {
+        if (!fadingOut)
+        {
+            fadingOut = true;
+            // StopAllCoroutines();
+            // StartCoroutine(FadeOutCoroutine());
+        }
+    }
+
+    // private IEnumerator FadeInCoroutine()
+    // {
+    //     while (transparency <= 1.0f)
+    //     {
+    //         transparency += Time.deltaTime;
+    //         SetFade(transparency);
+    //         yield return null;
+    //     }
+    //
+    //     transparency = 1.0f;
+    //     SetFade(transparency);
+    // }
+    //
+    // private IEnumerator FadeOutCoroutine()
+    // {
+    //     while (transparency >= minTransparency)
+    //     {
+    //         transparency -= Time.deltaTime;
+    //         SetFade(transparency);
+    //         yield return null;
+    //     }
+    //
+    //     transparency = minTransparency;
+    //     SetFade(transparency);
+    // }
+
+    private void SetFade(float t)
+    {
+        for (int i = 0; i < materials.Length; i++)
+        {
+            materials[i].SetFloat("_transparency", Mathf.Lerp(.5f, 1.5f, t));
         }
     }
 }
