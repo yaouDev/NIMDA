38c7cec54510ee46e23ab44d76d257c6919b4caf
yaouDev <augustjansson@outlook.com>
2022-05-16 16:07:01 +0200

    audio implementation

---
+++ SpelGrupp2/Assets/Scripts/caej/EnemyAudioContainer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+[CreateAssetMenu(fileName = "New Audio Container", menuName = "Audio/Audio Container/Enemy Audio Container")]
+public class EnemyAudioContainer : AudioContainer
+{
+    [Header("Combat")]
+    public EventReference fire1;
+    public EventReference melee;
+    public EventReference hurt;
+    public EventReference death;
+}
