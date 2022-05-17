43468322589d16b7b70f261fdc45aa4a7fd83a09
Boppan <38586588+Boppan@users.noreply.github.com>
2022-05-17 17:34:36 +0200

    SawnControllerDone

--- SpelGrupp2/Assets/Scripts/EnemySpawnController.cs
+++ SpelGrupp2/Assets/Scripts/EnemySpawnController.cs
@@ -6,23 +6,32 @@ public class EnemySpawnController : MonoBehaviour
 {
     [Header("What to Spawn")]
     [SerializeField] private GameObject[] spawnThis;
+    [SerializeField] private int maxSpawnThisMany = 5;
+    [SerializeField] private int minSpawnThisMany = 1;
+    private int spawnThisMany;
     [Header("Normal Spawn")]
     [SerializeField] private int nMaxSpawnCount = 25;
-    [SerializeField] private float nSpawnCooldown = 5;
+    [SerializeField] private int nMaxSpawnCooldown = 7;
+    [SerializeField] private int nMinSpawnCooldown = 4;
     [SerializeField] private int nSpawnDistanceMax = 35;
     [SerializeField] private int nSpawnDistanceMin = 5;
+    private int nSpawnCooldown;
     [Header("Night Spawn")]
     [SerializeField] private int nightMaxSpawnCount = 30;
-    [SerializeField] private float nightSpawnCooldown = 0.1f;
+    [SerializeField] private int nightMaxSpawnCooldown = 5;
+    [SerializeField] private int nightMinSpawnCooldown = 1;
     [SerializeField] private int nightSpawnDistanceMax = 35;
     [SerializeField] private int nightSpawnDistanceMin = 5;
+    private int nightSpawnCooldown;
     [Header("Generator Spawn")]
     [SerializeField] private int genMaxSpawnCount = 40;
-    [SerializeField] private float genSpawnCooldown = 0.1f;
+    [SerializeField] private int genMaxSpawnCooldown = 3;
+    [SerializeField] private int genMinSpawnCooldown = 1;
     [SerializeField] private int genSpawnDistanceMax = 50;
     [SerializeField] private int genSpawnDistanceMin = 15;
+    private int genSpawnCooldown;
     private int maxSpawnCount;
-    private float spawnCooldown;
+    private int spawnCooldown;
     private int spawnDistanceMax;
     private int spawnDistanceMin;
     private int spawnCount;
@@ -37,7 +46,6 @@ public class EnemySpawnController : MonoBehaviour
     private Transform spawnPos;
     private DayNightSystem dayNightSystem;
 
-    // Start is called before the first frame update
     void Start()
     {
         dayNightSystem = FindObjectOfType<DayNightSystem>();
@@ -49,16 +57,16 @@ public class EnemySpawnController : MonoBehaviour
     {
         IsNight();
     }
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     IEnumerator SpawnObject()
     {
         while (true)
         {
+            nSpawnCooldown = Random.Range(nMinSpawnCooldown, nMaxSpawnCooldown);
+            nightSpawnCooldown = Random.Range(nightMinSpawnCooldown, nightMinSpawnCooldown);
+            genSpawnCooldown = Random.Range(genMinSpawnCooldown, genMaxSpawnCooldown);
+            spawnThisMany = Random.Range(minSpawnThisMany, maxSpawnThisMany);
+
             if (spawnLocations.Length == 0)
             {
                 spawnLocations = GameObject.FindGameObjectsWithTag("SpawnLocation");
@@ -80,9 +88,12 @@ public class EnemySpawnController : MonoBehaviour
                 index = Random.Range(0, nearbySpawners.Count);
                 activeSpawner = nearbySpawners[index];
                 spawnPos = activeSpawner.transform;
-                Instantiate(spawnThis[Random.Range(0, 2)], spawnPos.position, spawnPos.rotation);
-                nearbySpawners.Clear();
-                spawnCount += 1;
+                for (int i = 0; i < spawnThisMany; i++)
+                {
+                    Instantiate(spawnThis[Random.Range(0, 2)], spawnPos.position, spawnPos.rotation);
+                    nearbySpawners.Clear();
+                    spawnCount += 1;
+                }
             }
             yield return new WaitForSeconds(spawnCooldown);
         }
@@ -118,7 +129,8 @@ public class EnemySpawnController : MonoBehaviour
             maxSpawnCount = nightMaxSpawnCount;
             spawnDistanceMax = nightSpawnDistanceMax;
             spawnDistanceMin = nightSpawnDistanceMin;
-        } else
+        }
+        else
         {
             spawnCooldown = nSpawnCooldown;
             maxSpawnCount = nMaxSpawnCount;
