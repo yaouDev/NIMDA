21dd2d7f71c852718612c565ea52b03cf943bc17
TheiPadCat <theipadcat@gmail.com>
2022-05-20 11:23:58 +0200

    lagfixxxx

--- SpelGrupp2/Assets/Scripts/BossSpawner.cs
+++ SpelGrupp2/Assets/Scripts/BossSpawner.cs
@@ -1,28 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CallbackSystem;
 
-public class BossSpawner : MonoBehaviour
-{
+public class BossSpawner : MonoBehaviour {
     [SerializeField] private GameObject boss;
     private int playerCount;
+    EnemySpawnController spawner;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        spawner = GameObject.FindObjectOfType<EnemySpawnController>();
         // boss = GameObject.Find("Boss");
     }
 
-    void OnTriggerEnter(Collider col)
-    {
-        if (col.gameObject.tag == "Player")
-        {
+    void OnTriggerEnter(Collider col) {
+        if (col.gameObject.tag == "Player") {
             playerCount++;
-            if (playerCount == 2)
-            {
+            if (playerCount == 2) {
                 boss.SetActive(true);
                 gameObject.SetActive(false);
+                EventSystem.Current.FireEvent(new SafeRoomEvent());
+                spawner.gameObject.SetActive(false);
             }
         }
     }
