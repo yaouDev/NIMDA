cc79e4b35a7f0824c6e038f3b0990708902d566d
CaretSoftware <pbergsten@me.com>
2022-05-23 14:37:13 +0200

    Camera Iteration

--- SpelGrupp2/Assets/Scripts/Camera/CameraController.cs
+++ SpelGrupp2/Assets/Scripts/Camera/CameraController.cs
@@ -150,7 +150,7 @@ public class CameraController : MonoBehaviour {
 
 		// TODO clamp centroid to magnitude
 		
-		// both cameras have the same rotation (fixed?)
+		// both cameras have the same rotation
 		
 		// the split is rotating freely between the players
 		RotateScreenSplit();
