16960ac274daca0a27096240ad857335332f44f7
TheiPadCat <theipadcat@gmail.com>
2022-06-02 19:20:06 +0200

    Update ChaseNode.cs

--- SpelGrupp2/Assets/Scripts/Scripts_Emil/AI_BehaviourTree/CustomNodes/ChaseNode.cs
+++ SpelGrupp2/Assets/Scripts/Scripts_Emil/AI_BehaviourTree/CustomNodes/ChaseNode.cs
@@ -7,23 +7,18 @@ public class ChaseNode : Node {
 
     [SerializeField] private float acceleration = 13.5f, maxSpeed = 10f;
     [SerializeField] private float distanceFromTargetToStop;
-    private Animator animator;
     public override NodeState Evaluate() {
-        animator = agent.GetComponent<Animator>();
         if (agent.Destination != agent.ClosestPlayer || (agent.Destination == agent.ClosestPlayer && agent.CurrentPath == null)) {
             agent.Acceleration = acceleration;
             agent.MaxSpeed = maxSpeed;
             agent.Destination = agent.ClosestPlayer;
-            animator.SetBool("IsChasing", true);
             agent.IsStopped = false;
 
             NodeState = NodeState.RUNNING;
         } else if (agent.CurrentPath != null && distanceFromTargetToStop < agent.DistanceFromTarget) {
             NodeState = NodeState.RUNNING;
-            animator.SetBool("IsChasing", true);
             agent.IsStopped = false;
         } else {
-            animator.SetBool("IsChasing", false);
             agent.IsStopped = true;
             NodeState = NodeState.SUCCESS;
         }
